5fc4566966481c172caccc2407d78b00980b786b
cairurui <cairurui@163.com>
2018-04-16 13:31:56 +0800

    手动写 AIDL 步骤三： * 添加 Proxy 类的具体实现：     1. getBooks 方法调用 Binder 的 transact 方法，交给 Binder 去找 另一个进程中对应的 Binder 对象     2. addBook 方法同样原理。】

--- messageclient/src/main/java/com/xiaocai/messageclient/ipc/proxy/Proxy.java
+++ messageclient/src/main/java/com/xiaocai/messageclient/ipc/proxy/Proxy.java
@@ -1,10 +1,12 @@
 package com.xiaocai.messageclient.ipc.proxy;
 
 import android.os.IBinder;
+import android.os.Parcel;
 import android.os.RemoteException;
 
 import com.xiaocai.messageclient.ipc.Book;
 import com.xiaocai.messageclient.ipc.server.BookManager;
+import com.xiaocai.messageclient.ipc.server.Stub;
 
 import java.util.List;
 
@@ -14,23 +16,59 @@ import java.util.List;
 
 public class Proxy implements BookManager {
 
+    public static final String DESCRIPTOR = "com.xiaocai.messageclient.ipc.server.BookManager";
+
+    IBinder mRemote;
+
 
     public Proxy(IBinder binder) {
+        this.mRemote = binder;
+    }
 
+    public String getInterfaceDescriptor() {
+        return DESCRIPTOR;
     }
 
     @Override
     public List<Book> getBooks() throws RemoteException {
-        return null;
+        Parcel data = Parcel.obtain();
+        Parcel replay = Parcel.obtain();
+        List<Book> result = null;
+        try {
+            data.writeInterfaceToken(DESCRIPTOR);
+            mRemote.transact(Stub.TRANSAVTION_getBooks, data, replay, 0);
+            replay.readException();
+            result = replay.createTypedArrayList(Book.CREATOR);
+        } finally {
+            data.recycle();
+            replay.recycle();
+        }
+        return result;
     }
 
     @Override
     public void addBook(Book book) throws RemoteException {
+        Parcel data = Parcel.obtain();
+        Parcel replay = Parcel.obtain();
+        try {
+            data.writeInterfaceToken(DESCRIPTOR);
+            if (book != null) {
+                data.writeInt(0);
+                book.writeToParcel(data, 0);
+            } else {
+                data.writeInt(0);
+            }
 
+            mRemote.transact(Stub.TRANSAVTION_addBook, data, replay, 0);
+            replay.readException();
+        } finally {
+            data.recycle();
+            replay.recycle();
+        }
     }
 
     @Override
     public IBinder asBinder() {
-        return null;
+        return mRemote;
     }
 }
