8aad5347c0acdad203c22528c6755eaa8cb9a06e
cairurui <cairurui@163.com>
2018-05-19 15:40:40 +0800

    + glide 源码分析

---
+++ app/src/main/java/com/charry/xandroid/ui/customview/ComplexUIActivity.java
@@ -0,0 +1,29 @@
+package com.charry.xandroid.ui.customview;
+
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+
+import com.alibaba.android.vlayout.VirtualLayoutManager;
+import com.charry.xandroid.R;
+
+public class ComplexUIActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_complex_ui);
+
+        recyclerView = findViewById(R.id.rv);
+
+        final VirtualLayoutManager layoutManager = new VirtualLayoutManager(this);
+
+        recyclerView.setLayoutManager(layoutManager);
+
+        RecyclerView.RecycledViewPool viewPool = new RecyclerView.RecycledViewPool();
+        recyclerView.setRecycledViewPool(viewPool);
+        viewPool.setMaxRecycledViews(0, 10);
+    }
+}
